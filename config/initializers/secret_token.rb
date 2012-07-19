fa15103e86fac1ae7ae7ea865e0039db110ade12
Radu Oprescu <runnaway90@yahoo.com>
2012-07-19 22:03:15 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DinosOnRails::Application.config.secret_token = '5623ea32780623b44b07d493e7fc950d5b8dd29dfcb0fe9eb433ce55d1989b12caeda97091bc4c4edbfd22082a7e0f0fcd552ba3083b376b0772adc36231fed1'

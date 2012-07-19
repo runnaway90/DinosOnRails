7c09031b2199b24c79ced4a4cf10d0899fec0ae0
Radu Oprescu <runnaway90@yahoo.com>
2012-07-19 22:34:33 +0100

    Database and Users

---
+++ db/20120719212437_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :username
+      t.string :password_hash
+      t.string :salt
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps
+    end
+  end
+end
